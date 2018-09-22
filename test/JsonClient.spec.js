8341fbd36b34b6d8647b160ebe086bda9bb84800
Sebastien Cadorette <cado.94@me.com>
2018-09-22 12:54:06 -0400

    JsonClient - Added the describe for the multiple function to test.

--- test/JsonClient.spec.js
+++ test/JsonClient.spec.js
@@ -4,18 +4,49 @@ import * as chai from 'chai';
 let expect = chai.expect;
 let assert = chai.assert;
 
-export default describe('JsonClient', () => {
+export default describe ('JsonClient', () => {
   let jsonClient;
 
   beforeEach(() => {
     jsonClient = new SharedBox.JsonClient('api', 1, 'endpoint');
   });
 
-  it('Should do nothing', () => {
+  it ('Should do nothing', () => {
     expect(true).to.be.true;
   });
 
-  it('Should be defined', () => {
+  it ('Should be defined', () => {
     assert(jsonClient);
+  });
+
+  describe('Constructor', () => {
+    it ('Should have initialized apiToken property with the good value', () => {
+      expect(jsonClient['apiToken']).to.deep.equal('api');
+    });
+    it ('Should have initialized userId property with the good value', () => {
+      expect(jsonClient['userId']).to.deep.equal(1);
+    });
+    it ('Should have initialized endpoint property with the good value', () => {
+      expect(jsonClient['endpoint']).to.deep.equal('endpoint');
+    });
+    it ('Should have initialized noCaching property with the good value', () => {
+      expect(jsonClient['noCaching']).to.be.false;
+    });
+  });
+
+  describe ('initializeSharedbox function', () => {
+
+  });
+
+  describe ('submitSharedbox function', () => {
+
+  });
+
+  describe ('addRecipient function', () => {
+
+  });
+
+  describe ('closeSharedbox function', () => {
+
   });
 });
