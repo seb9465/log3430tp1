1801ff428adcecd39302de8c8103e06816634eb4
Sebastien Cadorette <cado.94@me.com>
2018-09-22 13:25:22 -0400

    JsonClient - InitializeSharedbox - Stubbed the _makeRequest function and made sure it has been called with one parameter.

--- test/JsonClient.spec.js
+++ test/JsonClient.spec.js
@@ -1,14 +1,18 @@
 import SharedBox from '../src/sharedbox';
 import * as chai from 'chai';
+import sinon from 'sinon';
 
 let expect = chai.expect;
 let assert = chai.assert;
 
 export default describe ('JsonClient', () => {
   let jsonClient;
+  // let stub;
+  // let setStub = 
 
   beforeEach(() => {
     jsonClient = new SharedBox.JsonClient('api', 1, 'endpoint');
+
   });
 
   it ('Should do nothing', () => {
@@ -35,7 +39,19 @@ export default describe ('JsonClient', () => {
   });
 
   describe ('initializeSharedbox function', () => {
+    it ('Should call the _makeRequest function with one parameter', () => {
+      let makeRequestStub = sinon.stub(jsonClient, '_makeRequest').callsFake((userEmail) => {
+        return userEmail;
+      });
 
+      const someUserEmail = 'john.doe@email.com';
+      const expectedResult = `api/sharedboxes/new?email=${someUserEmail}`;
+
+      let result = jsonClient.initializeSharedBox(someUserEmail);
+
+      expect(result).to.deep.equal(expectedResult);
+      assert(makeRequestStub.calledOnceWith(expectedResult));
+    });
   });
 
   describe ('submitSharedbox function', () => {
