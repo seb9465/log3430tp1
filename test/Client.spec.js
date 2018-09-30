6848f8b4aeeaab689d141ebedfe508de961a6e36
SebastienCadorette <cado.94@me.com>
2018-09-30 16:42:21 -0400

    Client - Construtor - Regrouped some tests + suppressed some comments.

--- test/Client.spec.js
+++ test/Client.spec.js
@@ -9,35 +9,6 @@ let expect = chai.expect;
 let assert = chai.assert;
 
 export default describe('Client', () => {
-//   const SHAREDBOX = {
-//     'userEmail': 'user@acme.com',
-//     'guid': '1c820789a50747df8746aa5d71922a3f',
-//     'uploadUrl': 'upload_url',
-//     'recipients': [/* list of Recipient objects*/],
-//     'attachments': [/*list of Attachment objects*/],
-//     'message': 'lorem ipsum...',
-//     'subject': 'Donec rutrum congue leo eget malesuada.',
-//     'notificationLanguage': 'en',
-//     'securityOptions': {
-//       'allowRememberMe': true,
-//       'allowSms': true,
-//       'allowVoice': true,
-//       'allowEmail': true,
-//       'expirationValue': 5,
-//       'expirationUnit': 'days',
-//       'retentionPeriodType': 'do_not_discard',
-//       'retentionPeriodValue': null,
-//       'retentionPeriodUnit': 'hours',
-//       'allowManualClose': true
-//     },
-//     'userId': 1,
-//     'status': 'in_progress',
-//     'previewUrl': 'http://sharedbox.com/sharedboxes/dhjewg67ewtfg476/preview',
-//     'createdAt': '2018-05-24T14:45:35.062Z',
-//     'updatedAt': '2018-05-24T14:45:35.589Z',
-//     'expiration': '2018-05-31T14:45:35.038Z',
-//     'closedAt': null
-//   };
   let client;
 
   beforeEach(() => {
@@ -52,59 +23,22 @@ export default describe('Client', () => {
     it('should be defined', () => {
       assert(client);
     });
-    it('Should have initialized the apiToken with the right value', () => {
+    it('Should have initialized the properties with the right value', () => {
       expect(client['apiToken']).to.deep.equal('api');
-    });
-    it('Should have initialized the userId with the right value', () => {
       expect(client['userId']).to.deep.equal(1);
-    });
-    it('Should have initialized the endpoint with the right value', () => {
       expect(client['endpoint']).to.deep.equal('endpoint');
-    });
-    it('Should have initialized the jsonClient', () => {
       assert.isDefined(client['jsonClient']);
     });
   });
 
   describe('initializeSharedBox function', () => {
-    // it('Should have called the fetch method twice', () => {
-    //   let stub = sinon.stub(Utils, 'fetch').onCall(0).resolves({
-    //     status: 200,
-    //     text: () => {
-    //       return '';
-    //     },
-    //     json: () => {
-    //       return '';
-    //     },
-    //     statusText: '',
-    //     ok: true
-    //   });
-    //   stub.onCall(1).resolves({
-    //     text: () => {
-    //       return new Promise((resolve) => {
-    //         resolve({});
-    //       });
-    //     },
-    //     status: 200,
-    //     json: () => {
-    //       return '';
-    //     },
-    //     statusText: '',
-    //     ok: true
-    //   });
-
-    //   client.initializeSharedBox(SHAREDBOX).then(() => {
-    //     assert(stub.calledTwice);
-    //   });
-    //   stub.restore();
-    // });
   });
 
   describe('submitSharedbox function', () => {
     it('Should return exception if guid field is empty', () => {
       let sharedbox = new SharedBox.Helpers.Sharedbox();
 
-      assert.throws(function() {client.submitSharedBox(sharedbox);}, SharedBoxException);
+      assert.throws(function () { client.submitSharedBox(sharedbox); }, SharedBoxException);
     });
   });
 
